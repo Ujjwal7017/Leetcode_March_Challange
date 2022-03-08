0b0fab6b11d0ce352289e78eeb3d3f970fefdd5c
Ujjwal Shukla <73957886+Ujjwal7017@users.noreply.github.com>
2022-03-08 10:56:00 +0530

    Create Linked_List_Cycle.java

---
+++ Linked_List_Cycle.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+         if(head==null) return false;
+        
+        ListNode slow=head;
+        ListNode fast=head.next;
+        
+        while(slow!=fast){
+            slow=slow.next;
+            if(fast!=null && fast.next!=null)
+                fast=fast.next.next;
+            else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
